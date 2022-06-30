9deec787ac6f9b8e67ba6bb9202a410bf3c0d6cb
ShafayetZim <shafayet.zim@northsouth.edu>
2022-06-30 18:02:10 +0600

    model register files

--- dataset/admin.py
+++ dataset/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Courier, Origin, Destination, Shipper, Receiver
 
 # Register your models here.
+admin.site.register(Courier)
+admin.site.register(Origin)
+admin.site.register(Destination)
+admin.site.register(Shipper)
+admin.site.register(Receiver)
