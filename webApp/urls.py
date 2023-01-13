bdd4276da90a33b78f8915e64a263717c809eb54
ShafayetZim <shafayet.zim@northsouth.edu>
2023-01-13 17:29:28 +0600

    crud url for office address

--- webApp/urls.py
+++ webApp/urls.py
@@ -12,4 +12,8 @@ urlpatterns = [
     path('booking', views.new_booking, name="booking"),
     path('booking_list', views.BookingView.as_view(), name="booking-list"),
     path('booking_detail/<str:pk>', views.BookingDetailView.as_view(), name='booking-detail'),
+    path('new_office', views.new_office, name="new-office"),
+    path('office_list', views.OfficeView.as_view(), name="office-list"),
+    path('office_update/<str:pk>', views.OfficeUpdateView.as_view(), name='update-office'),
+    path('office_delete/<int:id>', views.office_delete, name="office-delete"),
 ]
