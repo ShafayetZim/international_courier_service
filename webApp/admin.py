526cf73d221a0e2a8b55d1501ab07a3d73281b92
ShafayetZim <shafayet.zim@northsouth.edu>
2023-01-17 18:51:24 +0600

    register new added models

--- webApp/admin.py
+++ webApp/admin.py
@@ -4,3 +4,10 @@ from .models import *
 # Register your models here.
 admin.site.register(Booking)
 admin.site.register(Office)
+admin.site.register(Service)
+admin.site.register(Gallery)
+admin.site.register(Partner)
+admin.site.register(Review)
+admin.site.register(About)
+admin.site.register(Facility)
+admin.site.register(Benefit)
